6a792679c28273a6cbefda8e4475202166fcf5cd
Đỗ Hoàng Long <longhoangdomelbie2k3@gmail.com>
2023-06-24 15:55:33 +0700

    building navbar

---
+++ blog-ai-app/app/api/hello/routes.ts
@@ -0,0 +1,3 @@
+export async function get(request: Request) {
+    return new Response ("Hello, Next.js!")
+}
---
+++ blog-ai-app/app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+})
+
+/*(metadata function)This title will appear in the tab bar, and description is about your website which is for Google search engine recognize your website*/
+export const metadata = {
+  title: 'Blog AI App',
+  description: 'Blog built in Next JS that uses AI',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className={openSans.className} lang="en">
+      <body>
+        <Navbar />
+        {children}
+        {/*<Footer /> */}
+      </body>
+    </html>
+  );
+}
